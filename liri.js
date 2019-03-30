e791e314f27757e2821119140d6b1e3d2b5f475a
alex chalyy <alexchalyy@gmail.com>
2019-03-29 21:39:34 -0400

    program description is added to liri.js

--- liri.js
+++ liri.js
@@ -1,5 +1,7 @@
 /*  This is liri bot js file.
 
+    This program is called by node in console to look up for conccerts/movies/songs. The API get call results are printed on the console.
+
     Written by Alex Chalyy on 3/29/2019.    */
 
 require("dotenv").config();
