8aafd541369c45999d4cffc725678c43fb7e5992
alex chalyy <alexchalyy@gmail.com>
2019-03-29 22:55:04 -0400

    packages installed, switch statement set up, concert function started

--- liri.js
+++ liri.js
@@ -5,5 +5,48 @@
     Written by Alex Chalyy on 3/29/2019.    */
 
 require("dotenv").config();
-
+const axios = require("axios");
 var keys = require("./keys.js");
+
+//--------------------------------------------------------------------------------
+
+app(process.argv[2], process.argv[3]);
+
+//--------------------------------------------------------------------------------
+
+function app(command, parameter) {
+
+    //  This statement checks for user input and performs proper action.
+
+    switch (command) {
+        case "concert-this":
+            showConcert(parameter);
+            break;
+     /*   case "spotify-this-song":
+            showSong(parameter);
+            break;
+        case "movie-this":
+            showMoive(parameter);
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break; */
+        default:
+            console.log("Liri does not know this command. Please try again.");
+    }
+}
+
+//--------------------------------------------------------------------------------
+
+function showConcert(parameter) {
+
+    var queryUrl = "https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp";
+
+    axios.get(queryUrl)
+        .then(function (response) {
+            console.log(response.data[0].venue.jason.stringify);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
