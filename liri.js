877db0d3a3b1de3b68110ad9cd2733b760b6f1c4
alex chalyy <alexchalyy@gmail.com>
2019-03-31 14:40:09 -0400

    spotify fully functional

--- liri.js
+++ liri.js
@@ -8,11 +8,29 @@ require("dotenv").config();
 const axios = require("axios");
 const moment = require("moment");
 var keys = require("./keys.js");
-moment().format();
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
 //--------------------------------------------------------------------------------
 
-app(process.argv[2], process.argv[3]);
+app(process.argv[2], MakeTitle());
+
+//--------------------------------------------------------------------------------
+
+function MakeTitle()    {
+
+//  This function creates title from user input for the search.
+
+    var c = 3;
+    var title = process.argv[3];
+
+    while (process.argv[c + 1]) {
+        title += " " + process.argv[c + 1];
+        c++;
+    }
+    
+    return title;
+}
 
 //--------------------------------------------------------------------------------
 
@@ -22,17 +40,20 @@ function app(command, parameter) {
 
     switch (command) {
         case "concert-this":
-            if (parameter)  {
+            if (parameter) {
                 showConcert(parameter);
             }
             else showConcert("drake");  // if user does not enter artist/band, drake concerts are looked up
             break;
-     /*   case "spotify-this-song":
-            showSong(parameter);
-            break;
+        case "spotify-this-song":
+            if (parameter) {
+                showSong(parameter);
+            }
+            else Sign();
+            break; 
         case "movie-this":
-            showMoive(parameter);
-            break;
+            showMovie(parameter);
+            break; /*
         case "do-what-it-says":
             doWhatItSays();
             break; */
@@ -45,7 +66,7 @@ function app(command, parameter) {
 
 function showConcert(parameter) {
 
-//  This function requests artist/band concert information and prints out venue name/location/date for top 5 results.
+    //  This function requests artist/band concert information and prints out venue name/location/date for top 5 results.
 
     var queryUrl = "https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp";
 
@@ -61,4 +82,61 @@ function showConcert(parameter) {
         .catch(function (error) {
             console.log(error);
         });
+}
+
+//--------------------------------------------------------------------------------
+
+function showSong(song) {
+
+    //  This function requests song information and prints artist(s), song name, a preview link of the song from spotify, and album.
+
+    spotify
+        .search({ type: 'track', query: song })
+        .then(function (response) {
+            for (var c = 0; c < 5; c++) {
+                console.log("\nArtist: " + response.tracks.items[c].album.artists[0].name);
+                console.log("Song: " + response.tracks.items[c].name);
+                console.log("Preview link: " + response.tracks.items[c].external_urls.spotify);
+                console.log("Album: " + response.tracks.items[c].album.name);
+            }
+            console.log("\n");
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+//--------------------------------------------------------------------------------
+
+function Sign() {
+
+    //  This function makes api spotify get call for Ace of Base "The Sign" song and prints out in case no input is added for search song name.
+
+    spotify
+        .search({ type: 'track', query: 'The Sign' })
+        .then(function (response) {
+            for (var c = 0; c < 100; c++) {
+                if (response.tracks.items[c]) {
+                    if (response.tracks.items[c].album.artists[0].name == "Ace of Base") {
+                        console.log("\nArtist: " + response.tracks.items[c].album.artists[0].name);
+                        console.log("Song: " + response.tracks.items[c].name);
+                        console.log("Preview link: " + response.tracks.items[c].external_urls.spotify);
+                        console.log("Album: " + response.tracks.items[c].album.name);
+                    }
+                }
+            }
+            console.log("\n");
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+//--------------------------------------------------------------------------------
+
+function showMovie(parameter)   {
+
+//  This function displays movie search results from OMDB API get call and displays 5 results including title, year, ratings, country, language, plot, and actors. 
+
+    console.log("here");
 }
